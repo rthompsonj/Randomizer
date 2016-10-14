34a1def733aa108743ae8158a91d043471911a6a
Robert Thompson <rthompsonj@gmail.com>
2016-10-14 12:06:02 -0500

    Initial commit of the randomizer

--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -8,18 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var inputField_a: UITextField!
+    @IBOutlet weak var inputField_b: UITextField!
+    @IBOutlet weak var inputField_c: UITextField!
+    @IBOutlet weak var inputField_d: UITextField!
 
+    var textFields = [UITextField]()
+    var textOptions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        textFields.append(inputField_a)
+        textFields.append(inputField_b)
+        textFields.append(inputField_c)
+        textFields.append(inputField_d)
+        
+        for field in textFields {
+            field.delegate = self
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
 
+    @IBAction func doTheRandom(_ sender: AnyObject) {
+        textOptions = []
+        
+        for field in textFields {
+            if !(field.text?.isEmpty == true){
+                textOptions.append(field.text!)
+            }
+        }
+        
+        if(textOptions.count <= 0){
+            return
+        }
+        
+        let index = arc4random_uniform(UInt32(textOptions.count))
+        let txt = textOptions[Int(index)]
+        resultLabel.text = txt
+    }
+
+    @IBAction func Reset(_ sender: AnyObject) {
+        for field in textFields {
+            field.text = ""
+        }
+    }
 
 }
 
